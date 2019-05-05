9fe3edd9aa5a8117090fc9b929142895d7f261f1
Jonathan Morales <jonmorazav@yahoo.com>
2019-05-05 01:20:23 -0400

    add working crypto util and pw encryption in react

--- src/Components/Signin/SigninForm/index.js
+++ src/Components/Signin/SigninForm/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import StandardInput from '../../../Atoms/Forms/TextInputField';
 import ModuletteButton from '../../../Atoms/StandardButton';
 import styled from 'styled-components';
+import crypto from '../../../utils/crypto';
+import axios from 'axios';
 
 const SubmitButton = styled(ModuletteButton)`
     margin-top: 10px;
@@ -38,108 +40,15 @@ class SigninForm extends React.Component {
         });
     }
 
-    handleSubmit(event) {
+    async handleSubmit(event) {
         event.preventDefault();
         const pw = this.state.password;
-        this.hashPassword(pw)
-            .then(hash => {
-                console.log('RAW HASH')
-                console.log(this.hashToHex(hash));
-                this.encryptHash(hash, "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAnomv/EVvXTDKoT3ZqU8wk3d2X9+71ChO9EVS/B8+3qwMiaRvfEpfWJd5QE5s4quA3A1scJ8iYvZnnBiPUWs4m/RCqt4fx4AvsZrE+rHX7psF1NoLOlxhwL8n7/SePBsqPvHSRPopXreXyqK2yNJw4FKJjDlhbskOh8v7nwIkf6QFm6rSQw3nUS8ACnzh38tfr0DLoWeFX5TZOyHVcxOWhQ9O5AEJqPMrJwMi2wT9XypnPfbHLif01/nyzZg4EPeD6y2FDWuigFnQ0P9dYdXMSKX1I2JO7XnLbwa1VXMU2o9wjJa3VG5Qlcj7y9xwkk61NRz+OTm3+UMhV7BoF2r/k9R6BjBzX1ur9UtTjfHmPxyDz2Mw4f5BU2LI0oI8NeqDdZtTAx4FkOAVUDfx88K0VV/Vwk1OOvfIx6C/MbgWTICo8sTVJW0Tc0RXNupK/yiovjxYmhGMaJ9bcqTA7GwLDO8rDdSJLXUZKEjjRSKdT9gA4Pug3imtVkxualTkxKxiibBHvGdtASrAOTJhsjubgYLV0NzlNovEX+aNlRqgtTGYpwIuSHKoMEA3xCpWMeLp1p5aecjasU3PAOtzXjjvU10P/jYXVycUvlIgSPbjuzO6hTqdjGl9t67AJlc6iQV/WsQHA1hB2S43iucqrq8xNOt+rT41gURK9PdDxucsX18CAwEAAQ==")
-            })
-            .catch(err => {
-                console.error(err);
-            });
-
+        const hash = await crypto.hashPassword(pw)
+        const spki = await axios.get('/nonce');
+        const encryption = await crypto.encryptHash(hash, spki);
+        axios.post('/login', encryption);
     }
-
-    hashPassword(str) {
-        const encoder = new TextEncoder();
-        const pwArrayBuffer = encoder.encode(str);
-        return window.crypto.subtle.digest('SHA-256', pwArrayBuffer)
-    }
-
-    hashToHex(buff) {
-        const byteArray = new Uint8Array(buff);
-        const hexCodes = [...byteArray].map(value => {
-            const hexCode = value.toString(16);
-            const paddedHexCode = hexCode.padStart(2, '0');
-            return paddedHexCode;
-        });
-        return hexCodes.join('');
-    }
-
-    str2ab(key) {
-        const str = window.atob(key);
-        const buf = new ArrayBuffer(str.length);
-        const bufView = new Uint8Array(buf);
-        for (let i = 0, strLen = str.length; i < strLen; i++) {
-            bufView[i] = str.charCodeAt(i);
-        }
-        return buf;
-    }
-
-    importKey(spki) {
-        const binaryDer = this.str2ab(spki);
-        return window.crypto.subtle.importKey(
-            "spki",
-            binaryDer,
-            {
-                name: "RSA-OAEP",
-                hash: "SHA-256"
-            },
-            true,
-            ["encrypt"]
-        );
-    }
-
-    async encryptHash(hash, spki) {
-        console.log("IMPORTING KEY:")
-        console.log(spki)
-        this.importKey(spki)
-            .then(key => {
-                console.log("IMPORTED KEY:")
-                console.log(key)
-                console.log("ENCRYPTING")
-                window.crypto.subtle.encrypt(
-                    {
-                        name: "RSA-OAEP",
-                        iv: window.crypto.getRandomValues(new Uint8Array(12)),
-                    },
-                    key,
-                    hash
-                )
-                    .then(encrypted => {
-                        console.log("ENCRYPTED HASH")
-                        console.log(this.hashToHex(encrypted));
-                        this.decryptHash(encrypted)
-                    })
-                    .catch(err => {
-                        console.error(err);
-                    });
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    }
-
-    decryptHash(encryptedData) {
-        window.crypto.subtle.decrypt(
-            {
-                name: "AES-OAEP",
-                iv: new ArrayBuffer(12)
-            },
-            "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAnomv/EVvXTDKoT3ZqU8wk3d2X9+71ChO9EVS/B8+3qwMiaRvfEpfWJd5QE5s4quA3A1scJ8iYvZnnBiPUWs4m/RCqt4fx4AvsZrE+rHX7psF1NoLOlxhwL8n7/SePBsqPvHSRPopXreXyqK2yNJw4FKJjDlhbskOh8v7nwIkf6QFm6rSQw3nUS8ACnzh38tfr0DLoWeFX5TZOyHVcxOWhQ9O5AEJqPMrJwMi2wT9XypnPfbHLif01/nyzZg4EPeD6y2FDWuigFnQ0P9dYdXMSKX1I2JO7XnLbwa1VXMU2o9wjJa3VG5Qlcj7y9xwkk61NRz+OTm3+UMhV7BoF2r/k9R6BjBzX1ur9UtTjfHmPxyDz2Mw4f5BU2LI0oI8NeqDdZtTAx4FkOAVUDfx88K0VV/Vwk1OOvfIx6C/MbgWTICo8sTVJW0Tc0RXNupK/yiovjxYmhGMaJ9bcqTA7GwLDO8rDdSJLXUZKEjjRSKdT9gA4Pug3imtVkxualTkxKxiibBHvGdtASrAOTJhsjubgYLV0NzlNovEX+aNlRqgtTGYpwIuSHKoMEA3xCpWMeLp1p5aecjasU3PAOtzXjjvU10P/jYXVycUvlIgSPbjuzO6hTqdjGl9t67AJlc6iQV/WsQHA1hB2S43iucqrq8xNOt+rT41gURK9PdDxucsX18CAwEAAQ==",
-            encryptedData
-        )
-            .then(decrypted => {
-                console.log("HASH AGAIN")
-                console.log(this.hashToHex(decrypted));
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    }
+    
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
---
+++ src/utils/crypto.js
@@ -0,0 +1,108 @@
+export default {
+    hashPassword: function (str) {
+        const encoder = new TextEncoder();
+        const pwArrayBuffer = encoder.encode(str);
+        return window.crypto.subtle.digest('SHA-256', pwArrayBuffer)
+    },
+
+    hashToHex: function (buff) {
+        const byteArray = new Uint8Array(buff);
+        const hexCodes = [...byteArray].map(value => {
+            const hexCode = value.toString(16);
+            const paddedHexCode = hexCode.padStart(2, '0');
+            return paddedHexCode;
+        });
+        return hexCodes.join('');
+    },
+
+    strToArrBuff: function (str) {
+        const b64 = window.atob(str);
+        const buf = new ArrayBuffer(b64.length);
+        const bufView = new Uint8Array(buf);
+        for (let i = 0, b64Len = b64.length; i < b64Len; i++) {
+            bufView[i] = b64.charCodeAt(i);
+        }
+        return buf;
+    },
+
+    importPublicKey: function (spki) {
+        const binaryDer = this.strToArrBuff(spki);
+        return window.crypto.subtle.importKey(
+            "spki",
+            binaryDer,
+            {
+                name: "RSA-OAEP",
+                hash: "SHA-256"
+            },
+            false,
+            ["encrypt"]
+        );
+    },
+
+    importPrivateKey: function (pkcs8) {
+        const binaryDer = this.strToArrBuff(pkcs8);
+        return window.crypto.subtle.importKey(
+            "pkcs8",
+            binaryDer,
+            {
+                name: "RSA-OAEP",
+                modulusLength: 4096,
+                publicExponent: new Uint8Array([1, 0, 1]),
+                hash: "SHA-256",
+            },
+            false,
+            ["decrypt"]
+        );
+    },
+    
+    encryptHash: async function (hash, spki) {
+        try {
+            const key = await this.importPublicKey(spki)
+            const iv = window.crypto.getRandomValues(new Uint8Array(12));
+            try {
+                const encryptedHash = await window.crypto.subtle.encrypt(
+                    {
+                        name: "RSA-OAEP",
+                        iv: iv,
+                    },
+                    key,
+                    hash
+                );
+                return { encryptedHash, iv };
+            }
+            catch (encryptError) {
+                console.log(encryptError)
+                Promise.reject(new Error(encryptError));
+            }
+        }
+        catch (importError) {
+            console.log(importError)
+            Promise.reject(new Error(importError));
+        }
+    },
+
+    decryptHash: async function (vector, pkcs8, data) {
+        try {
+            const key = await this.importPrivateKey(pkcs8);
+            try {
+                const decrypted = await crypto.subtle.decrypt(
+                    {
+                        name: "RSA-OAEP",
+                        iv: vector
+                    },
+                    key,
+                    data
+                );
+                return decrypted;
+            }
+            catch (decryptError) {
+                console.log(decryptError)
+                Promise.reject(new Error(decryptError));
+            }
+        }
+        catch (importError) {
+            console.log(importError)
+            Promise.reject(new Error(importError));
+        }
+    }
+};
